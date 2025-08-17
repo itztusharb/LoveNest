2858eeed0c9c9a4c64b0a5d4577d0bbb1733f80b
Tushar Bunger <tusharbunger@gmail.com>
2025-08-17 13:09:42 +0000

    Sure

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type {NextConfig} from 'next';
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -25,4 +31,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#f9d5d5" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
