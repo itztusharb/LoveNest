0258bae232c0dafdc3b0c301a2d6db9a5e2482e7
Tushar Bunger <tusharbunger@gmail.com>
2025-08-17 07:31:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -18,8 +18,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
         </Sidebar>
         <SidebarInset className="bg-background">
           <Header />
-          <div className="h-16" />
-          <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
+          <main className="flex-1 p-4 pt-20 sm:p-6 sm:pt-22 lg:p-8 lg:pt-24">{children}</main>
         </SidebarInset>
       </div>
     </SidebarProvider>
